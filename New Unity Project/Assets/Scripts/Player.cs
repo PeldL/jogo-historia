c4a93a0ceaf7170ff4397ab0c3e2b371e361fd7b
PeldL <gabrielgd341@gmail.com>
2024-10-03 13:52:03 -0300

    sf

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    public UnityEvent OnPlayerKillEnemy;
+    public UnityEvent OnPause;
+    public UnityEvent OnUnPause;
+
+    public int score;
+    public int life = 3;
+    public int lifeMax;
+    
+    public Transform foot;
+    bool groundCheck;
+    public float speed = 5, jumpStrength = 5;
+    float horizontal;
+    public Rigidbody2D body;
+
+    Collider2D footCollision;
+    int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+        //groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
+        footCollision = Physics2D.OverlapCircle(foot.position, 0.05f);
+        groundCheck = footCollision;
+        
+
+
+        if (Input.GetButtonDown("Jump") && groundCheck)
+        {
+            body.AddForce(new Vector2(0, jumpStrength * 100));
+        }
+        if(horizontal != 0)//Para GetAxisRaw
+        {
+            direction = (int)horizontal;
+        }
+        /*Para quem está usando GetAxis
+        if(horizontal < 0)
+        {
+            direction = -1;
+        } else if(horizontal > 0)
+        {
+            direction = 1;
+        }
+        */
+       
+
+     
+    
+    
+            
+        }
+    }
+   
