06edb9bbd8b6e799cdbbd4877e90f9bc08632d49
PeldL <gabrielgd341@gmail.com>
2024-10-08 15:29:21 -0300

    da

---
+++ New Unity Project/Assets/PlayerCombat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play an attack animation
+        // Detect enemies in range of attack
+        //Damage them
+
+
+    }
+
+}
