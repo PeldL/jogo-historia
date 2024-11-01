5a7b155c1490a745ef667699e5efff3f2b941c9d
PeldL <gabrielgd341@gmail.com>
2024-11-01 09:38:05 -0300

    sad

--- jogo historia/Assets/Scripts/Casa.cs
+++ jogo historia/Assets/Scripts/Casa.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Casa : MonoBehaviour
 {
     public float life = 100;
+    public Slider vuoida;
 
     // Start is called before the first frame update
     void Start()
     {
-
+         vuoida.value = 100;
     }
     // Update is called once per frame
 
@@ -18,6 +20,7 @@ public class Casa : MonoBehaviour
 
     void Update()
     {
+        vuoida.value = life;    
         if(life <= 0)
         {
             Destroy(gameObject);
--- jogo historia/Assets/Scripts/Enemy/EnemyManager.cs
+++ jogo historia/Assets/Scripts/Enemy/EnemyManager.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class EnemyManager : MonoBehaviour
 {
     public int enemyKilled;
+    public Text textoEnemy;
+    public void Start()
+    {
+        textoEnemy.text = "Derrotados: " + enemyKilled;
+    }
     public void EnemyKilled()
     {
         enemyKilled++;
-
+        textoEnemy.text = "Derrotados: " + enemyKilled;
         if (enemyKilled >= 30)
         {
+
             SceneManager.LoadSceneAsync(4);
             Debug.Log(enemyKilled);
         }
